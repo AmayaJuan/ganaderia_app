7e242413c9b02f96c20c383cec4c86160f8d90c0
Juan Pablo Velez Amaya <jpva360@gmail.com>
2026-06-04 22:49:47 -0500

    act

--- lib/features/lotes/presentation/pages/lotes_page.dart
+++ lib/features/lotes/presentation/pages/lotes_page.dart
@@ -307,7 +307,7 @@ class _LotesPageState extends State<LotesPage> {
               : ListView.separated(
                   padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
                   itemCount: _lotes.length,
-                  separatorBuilder: (_, __) => const SizedBox(height: 12),
+                  separatorBuilder: (_, _) => const SizedBox(height: 12),
                   itemBuilder: (ctx, i) => _buildLoteCard(_lotes[i]),
                 ),
         ),
