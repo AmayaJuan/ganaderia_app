407715679928b6355db3220c0e26a72c0220336a
Juan Pablo Velez Amaya <jpva360@gmail.com>
2026-06-07 00:02:56 -0500

    fix: ConnectivityService sin warnings

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "329187648" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3976756324" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
